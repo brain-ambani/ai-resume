0024cf7e5c89e32772489ab7f1a8da593e21dae2
brian-ambani <brainambani1@gmail.com>
2024-05-22 13:03:46 +0300

    completed the features page

--- components/Features.tsx
+++ components/Features.tsx
@@ -1,13 +1,11 @@
-import { CardFeature } from "./CardFeaure";
+import { FeaturesDetails } from "./FeatureDetails";
 
 export function Features() {
   return (
     <div className="w-3/4 mx-auto text-center">
       <h1 className="text-2xl "> This is the features section</h1>
       <div className="my-12 flex gap-4">
-        <CardFeature />
-        <CardFeature />
-        <CardFeature />
+        <FeaturesDetails />
       </div>
     </div>
   );
