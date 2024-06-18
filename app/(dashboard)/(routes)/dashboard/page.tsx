8bca34aabf67ffc8262a87e85e13889815310b33
brain-ambani <brainambani1@gmail.com>
2024-06-18 13:57:29 +0300

    updated the dashboard page

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,3 +1,18 @@
 export default function Home() {
-  return <main>This is the dashboard</main>;
+  return (
+    <div>
+      <div className="mb-8 space-y-4">
+        <h2 className="text-2xl md:text-4xl font-bold text-center">
+          Build your Resume with Genius AI
+        </h2>
+        <p
+          className="text-muted-foreground font-light text-sm
+        md:text-lg text-center"
+        >
+          Create a professional resume in minutes with our AI-powered resume
+          builder.
+        </p>
+      </div>
+    </div>
+  );
 }
