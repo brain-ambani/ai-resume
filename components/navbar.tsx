07b4d37a54548a2c46f5cdb8e5df6d2a5ce38113
brain-ambani <brainambani1@gmail.com>
2024-06-18 13:44:45 +0300

    completed the side nav

---
+++ components/navbar.tsx
@@ -0,0 +1,10 @@
+import MobileSidebar from "@/components/mobile-sidebar";
+
+const Navbar = () => {
+  return (
+    <nav className="flex items-center p-4">
+      <MobileSidebar />
+    </nav>
+  );
+};
+export default Navbar;
