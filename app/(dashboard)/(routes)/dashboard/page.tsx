85595c7eea77f138e253096746d3a17ecfda3a5a
brain-ambani <brainambani1@gmail.com>
2024-06-29 18:05:29 +0300

    completed the authentication using clerk

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,5 +1,4 @@
 import ResumeCard from "@/components/resume-card";
-import { UserButton, UserProfile } from "@clerk/nextjs";
 
 export default function Home() {
   return (
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,9 +1,13 @@
 import MobileSidebar from "@/components/mobile-sidebar";
+import { UserButton } from "@clerk/nextjs";
 
 const Navbar = () => {
   return (
-    <nav className="flex items-center p-4">
+    <nav className="flex items-center p-4 relative">
       <MobileSidebar />
+      <div className="absolute top-5 right-4">
+        <UserButton />
+      </div>
     </nav>
   );
 };
