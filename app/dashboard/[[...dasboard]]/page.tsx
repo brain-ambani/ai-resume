1ce3153c818769e3d0bcb8bdd01d58254f407653
brain-ambani <brainambani1@gmail.com>
2024-07-20 10:16:30 +0300

    updated the protected router, connected to db and updated the models

--- app/dashboard/[[...dasboard]]/page.tsx
+++ app/dashboard/[[...dasboard]]/page.tsx
@@ -1,14 +1,15 @@
 import { currentUser } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
 
-const Dashboard = () => {
-  const user = currentUser();
+const Dashboard = async () => {
+  const user = await currentUser();
 
   if (!user) {
     redirect("/");
   }
   return (
     <div>
+      <p>Welcome, {user?.firstName}</p>
       <h2>This is the dashboard page</h2>
     </div>
   );
