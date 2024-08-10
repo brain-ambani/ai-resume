1fea5d66ba45d2b3775398e8947d0a97ef383aed
brain ambani <brainambani1@gmail.com>
2024-08-10 22:35:18 +0300

    added the darkmode toggle

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,10 @@
+import { ModeToggle } from "@/components/themeToggle";
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
-  return <main>This is a new project</main>;
+  return <main className="flex gap-6">
+    <p>This is a saas project</p>
+    <Button>Get Started</Button>
+    <ModeToggle/>
+  </main>;
 }
