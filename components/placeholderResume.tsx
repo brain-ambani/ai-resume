1bff6afd04f49a945a72cdadd7f5374e2129c1d6
brain ambani <brainambani1@gmail.com>
2024-08-19 21:08:55 +0300

    updated the modal when user clicks on create resume

--- components/placeholderResume.tsx
+++ components/placeholderResume.tsx
@@ -4,22 +4,51 @@ import { Button } from "./ui/button";
 import { useRouter } from "next/navigation";
 import Link from "next/link";
 
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { useState } from "react";
+import { Input } from "./ui/input";
+
 export default function PlaceholderResume() {
   const router = useRouter();
 
+  const [openDialog, setOpenDialog] = useState(false);
   const handleClick = () => {
+    setOpenDialog(true);
     // Check if user is Free tier and push to upgrade page, else create the title of the resume and push to the create resume page
   };
   return (
-    <Button
-      asChild
-      onClick={handleClick}
-      className="flex flex-col items-center w-64 h-80 rounded-xl bg-gray-200 drop-shadow-md text-gray-400"
-    >
-      <Link href="/dashboard/create">
+    <div>
+      <Button
+        onClick={handleClick}
+        className="flex flex-col items-center w-64 h-80 rounded-xl bg-gray-200 drop-shadow-md text-gray-400"
+      >
         <PlusCircleIcon className="h-16 w-16" />
         <p>Create Resume</p>
-      </Link>
-    </Button>
+      </Button>
+      <Dialog open={openDialog}>
+        <DialogContent>
+          <DialogHeader>
+            <DialogTitle>Create New Resume</DialogTitle>
+            <DialogDescription>
+              <p>Add title for your new resume</p>
+              <Input className="my-2" placeholder="Ex.Full Stack Resume" />
+            </DialogDescription>
+            <div className="flex justify-end gap-5">
+              <Button variant="ghost" onClick={() => setOpenDialog(false)}>
+                Cancel
+              </Button>
+              <Button>Create</Button>
+            </div>
+          </DialogHeader>
+        </DialogContent>
+      </Dialog>
+    </div>
   );
 }
