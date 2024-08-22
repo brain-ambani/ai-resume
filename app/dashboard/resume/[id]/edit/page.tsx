46201218211c4d8cab6872952aa9cd4649a226f4
brain ambani <brainambani1@gmail.com>
2024-08-22 14:49:44 +0300

    to work on edit resume page

--- app/dashboard/resume/[id]/edit/page.tsx
+++ app/dashboard/resume/[id]/edit/page.tsx
@@ -1,6 +1,8 @@
 "use client";
 import { useParams } from "next/navigation";
 import { useEffect } from "react";
+import FormSection from "@/app/dashboard/resume/components/FormSection";
+import ResumePreview from "@/app/dashboard/resume/components/ResumePreview";
 
 export default function EditResume() {
   const params = useParams();
@@ -8,5 +10,12 @@ export default function EditResume() {
   useEffect(() => {
     // console.log(params.id);
   }, []);
-  return <div>This is the edit resume page</div>;
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-2 p-10 gap-10">
+      {/* Form section */}
+      <FormSection />
+      {/* Preview section */}
+      <ResumePreview />
+    </div>
+  );
 }
---
+++ app/dashboard/resume/components/ResumePreview.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ResumePreview = () => {
+  return <div>ResumePreview</div>;
+};
+
+export default ResumePreview;
